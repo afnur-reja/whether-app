48cd7273b6ba36a3f3e7b44b343fbabe6c53da78
Afnur Reja <afnurreja884@gmail.com>
2025-08-03 18:09:06 +0530

    update file

--- app.js
+++ app.js
@@ -5,3 +5,6 @@ const temperature = document.querySelector('.temp');
 const city = document.querySelector('.city');
 const humidity = document.querySelector('.humidity');
 const windSpeed = document.querySelector('.wind');
+
+const API_KEY = "c334edcac4913d646a0f3500ab778e0b";
+const url = `https://api.openweathermap.org/data/2.5/weather?q={city_name}&appid={API_KEY}&units=metric`
