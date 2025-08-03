a8ab8bd9d2aedab232be475051b8a95c78ddc0f8
Afnur Reja <afnurreja884@gmail.com>
2025-08-03 17:55:56 +0530

    add app.js file

---
+++ app.js
@@ -0,0 +1,7 @@
+const cityName = document.querySelector('#city-name');
+const search = document.querySelector('#search');
+const weatherIcon = document.querySelector('.weather-icon');
+const temperature = document.querySelector('.temp');
+const city = document.querySelector('.city');
+const humidity = document.querySelector('.humidity');
+const windSpeed = document.querySelector('.wind');
